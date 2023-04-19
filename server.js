0864b3fcbb82911f1f8e4af47186d36a7fcfbd6b
parmar varun <99722142+pv991@users.noreply.github.com>
2023-04-19 23:00:35 +0530

    ??

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ import {fileURLToPath} from 'url';
 dotenv.config();
 
 //database config
-connectDB().then(() => {
-    console.log("db connected");
-    app.listen(port, () => {
-        console.log("listening for requests");
-    })
+connectDB();
 })//esmoudle 6
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
